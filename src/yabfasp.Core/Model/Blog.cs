18dd2561a0a090e017494e44f348c505b542f6fe
James Dibble <james@jdibble.co.uk>
2015-10-30 20:28:39 +0000

    Adding basic domain nodel

---
+++ src/yabfasp.Core/Model/Blog.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Blog.cs" company="James Dibble">
+// Copyright (c) James Dibble. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Yabfasp.Core.Model
+{
+    using System;
+    using Dibble.Framework.Data;
+
+    /// <summary>
+    /// A data model of a blog.
+    /// </summary>
+    public class Blog : UniqueObject<Guid>
+    {
+        /// <summary>
+        /// Gets or sets the name of this <see cref="Blog"/>.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
